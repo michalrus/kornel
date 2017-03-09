1a1660f154fd71bba1c3d8bb6244fa558d9910b3
Michal Rus <m@michalrus.com>
2017-03-09 22:33:58 +0100

    Handle each message in its own thread

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,15 @@ module Main where
 import System.IO
 import System.IO.Error
 import Control.Monad
+import Control.Monad.Loops (whileJust_, iterateWhile)
+import Control.Concurrent (forkIO, threadDelay, killThread)
+import Control.Concurrent.Chan
 import Data.Maybe (maybeToList)
 import Data.Text as Text -- bug in Intero?
 import Data.Text.Encoding (decodeUtf8With, encodeUtf8)
 import qualified Data.Text.IO as TIO
+import qualified Data.UUID.V4 as UUID
+import qualified Data.UUID as UUID
 import Network.Connection
 import qualified IrcParser as I
 import CLI
@@ -22,11 +27,37 @@ main = do
 runConfig :: Config -> IO ()
 runConfig cfg = do
   ctx <- initConnectionContext
-  con <- login cfg ctx
-  catchIOError (forever $ processRawLine cfg con)
-    (\e -> if isEOFError e then return () else ioError e)
+  forever $ runSession cfg ctx
 
-login :: CLI.Config -> ConnectionContext -> IO Connection
+data IpcQueue
+  = QServerLine I.IrcLine
+  | QQuit
+  | QClientLine I.IrcCommand
+
+runSession :: Config -> ConnectionContext -> IO ()
+runSession cfg ctx = do
+  con <- login cfg ctx
+  ipc <- newChan
+  -- parse & enqueue lines from server
+  _ <- forkIO $ do
+    whileJust_ (processRawLine con) $ writeChan ipc . QServerLine
+    writeChan ipc QQuit
+  -- send our PINGs
+  let pingEvery = 20 * 1000 * 1000
+  thrPing <- forkIO $ forever $ do
+    threadDelay pingEvery
+    uuid <- UUID.nextRandom
+    writeChan ipc $ QClientLine $ I.Ping $ pack $ UUID.toString uuid
+  -- process the queue
+  _ <- iterateWhile id $ do
+    readChan ipc >>= \case
+      QQuit -> return False
+      QClientLine cmd -> sendCommand con cmd >> return True
+      QServerLine ln -> (forkIO $ handleLine cfg ln >>= mapM_ (writeChan ipc . QClientLine)) >> return True
+  killThread thrPing
+  return ()
+
+login :: Config -> ConnectionContext -> IO Connection
 login cfg ctx = do
   con <- connectTo ctx $ ConnectionParams
                             { connectionHostname  = serverHost cfg
@@ -51,18 +82,18 @@ login cfg ctx = do
     ] ++ maybeToList nickservCmd
   return con
 
-processRawLine :: Config -> Connection -> IO ()
-processRawLine cfg con = do
-  raw <- dropWhileEnd isEndOfLine <$> (decodeUtf8With $ \_ _ -> Just '_') <$> connectionGetLine 512 con
-  case I.readMessage raw of
-    Left  err -> hPutStrLn stderr $ "Failed to parse message ‘" ++ show raw ++ "’ with ‘" ++ show err ++ "’"
-    Right ok  -> do
-      response <- handleLine cfg ok
-      case response of
-        Nothing -> return ()
-        Just r -> sendCommand con r
-  where
-    isEndOfLine c = c == '\r' || c == '\n'
+processRawLine :: Connection -> IO (Maybe I.IrcLine)
+processRawLine con =
+  flip catchIOError (\e -> if isEOFError e then return Nothing else ioError e) $ do
+    rawBytes <- connectionGetLine 1024 con
+    raw <- dropWhileEnd isEndOfLine <$> (decodeUtf8With $ \_ _ -> Just '_') <$> pure rawBytes
+    case I.readMessage raw of
+      Left  err -> do
+        hPutStrLn stderr $ "Failed to parse message ‘" ++ show raw ++ "’ with ‘" ++ show err ++ "’"
+        return Nothing
+      Right ok  -> return $ Just ok
+    where
+      isEndOfLine c = c == '\r' || c == '\n'
 
 sendCommand :: Connection -> I.IrcCommand -> IO ()
 sendCommand con cmd = do
