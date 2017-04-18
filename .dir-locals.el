451f2ad3a381884e4f598ee8e8a05614ea298af2
Michal Rus <m@michalrus.com>
2017-04-18 23:37:45 +0200

    Use global intero-nix-shim

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,1 @@
-((haskell-mode
-  . ((eval . (setq intero-stack-executable
-                   (with-temp-buffer
-                     (cd (locate-dominating-file default-directory ".dir-locals.el"))
-                     (substring
-                      (shell-command-to-string
-                       "nix-shell --quiet --pure --run 'command -v intero-nix-shim' 2>&1 | tail -n 1")
-                      0 -1)))))))
+((nil . ((intero-stack-executable . "intero-nix-shim"))))
