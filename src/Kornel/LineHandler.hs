100d20772667e097deed80ab9bc46e5aa06251ed
Michal Rus <m@michalrus.com>
2017-10-07 01:35:18 +0200

    Change User-Agent to a real one
    E.g. YouTube doesn’t return correct <title/> when we’re pretending to
    be Chromium.

--- src/Kornel/LineHandler.hs
+++ src/Kornel/LineHandler.hs
@@ -103,9 +103,8 @@ fakeChromium r =
   { requestHeaders =
       [ ("Accept-Language", "en-US,en;q=0.8")
       , ( "Accept"
-        , "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-      , ( "User-Agent"
-        , "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+        , "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+      , ("User-Agent", "kornel +https://github.com/michalrus/kornel")
       ]
   }
 
