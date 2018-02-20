026f7fae75743ba696b08fbcedf1da8f043314e8
Michal Rus <m@michalrus.com>
2018-02-20 02:17:41 +0100

    Refactor default.nix

--- default.nix
+++ default.nix
@@ -1,50 +1,74 @@
 let
 
-  nixpkgs = (import <nixpkgs> {}).pkgs.fetchFromGitHub {
-    owner = "NixOS"; repo = "nixpkgs";
-    rev = "f0fac3b578086066b47360de17618448d066b30e";
-    sha256 = "1mpwdminwk1wzycwmgi2c2kwpbcfjwmxiakn7bmvvsaxb30gwyyb";
-  };
-
-in with import nixpkgs {}; let
-
-  compiler = haskell.packages.ghc802;
-
-  fmtInputs = (with haskellPackages; [ hlint hindent stylish-haskell ]);
-
-  # TODO: Consider using `cabal sdist`? https://git.io/vSo8l
-
-  build = let
-    src = builtins.filterSource (path: type: # FIXME: How to re-use .gitignore? https://git.io/vSo80
-         lib.all (i: toString i !=            path) [ ./.git ./dist ./result ]
-      && lib.all (i:          i != baseNameOf path) [ ".stack-work" ])
-      ./.;
-  in lib.overrideDerivation (compiler.callCabal2nix "kornel" src {}) (oldAttrs: {
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ fmtInputs;
-    preBuild = ''
-      hashRaw=$(${nix}/bin/nix-hash --type sha256 .)
-      make _autoformat
-      rm -r dist/autoformat
-      hashFmt=$(${nix}/bin/nix-hash --type sha256 .)
-      [ "$hashRaw" = "$hashFmt" ] || { echo >&2 'fatal: a file was commited unformatted' ; exit 1 ; }
-    '';
-    postBuild = ''
-      hlint .
-    '';
-  });
-
-  env = lib.overrideDerivation build.env (oldAttrs: {
-    buildInputs = fmtInputs ++ (with compiler; [ cabal-install ]);
-  });
-
-  hie = let
-    hie-nix = fetchFromGitHub {
+  sources = rec {
+    nixpkgs = import ./fetchNixpkgs.nix {
+      rev = "f0fac3b578086066b47360de17618448d066b30e";
+      sha256 = "0k0nxp13qdg53rcgr804529bnphqsb5v7rwzn47qq64nb147h011";
+    };
+    nixpkgsWatchexec = import ./fetchNixpkgs.nix {
+      rev = "1bccb28904ff1c1ea2fb6278fc950ebd5c8aed1d";
+      sha256 = "0mvvr0wr33rhb3ay1xiakq7dgm3jxkdjfjkm79gphb52l73cfig5";
+    };
+    hie-nix = (import nixpkgs {}).fetchFromGitHub {
       owner = "domenkozar"; repo = "hie-nix";
       rev = "2b7965f26009b43ecd65c2dcb4d15a53941b726e";
       sha256 = "1b2mv9pvbzk0fy1zjchfmkayya9dg1kq4xk0dqm9bzphz2f4icsv";
     };
-  in (import hie-nix {}).hie80.overrideAttrs (oldAttrs: {
-    postInstall = "echo ${hie-nix} >$out/prevent-ifd-gc";
+  };
+
+  ulib = { lib, ... }: {
+    sourceByNegativeRegex = regexes: src:
+      builtins.filterSource (path: type:
+         let relPath = lib.removePrefix (toString src) (toString path);
+         in lib.all (re: builtins.match re relPath == null) regexes) src;
+
+    # TODO: How to fully re-use recursive .gitignore’s? https://git.io/vSo80
+    gitignoreToRegexes = gitignorePath:
+      builtins.map (line: if lib.hasPrefix "/" line then line else ".*/" + line + "($|/.*)")
+        (builtins.map (builtins.replaceStrings ["." "**" "*"] ["\\." ".*" "[^/]*"] )
+           (builtins.map (lib.removeSuffix "/")
+              (lib.filter (line: line != "" && !(lib.hasPrefix "#" line))
+                 (lib.splitString "\n" (builtins.readFile gitignorePath)))));
+  };
+
+  watchexec = (import sources.nixpkgsWatchexec {}).watchexec;
+
+  hie = (import sources.hie-nix {}).hie80;
+
+in with (import sources.nixpkgs {}); with (ulib pkgs); let
+
+  haskellPackagesWithOverrides = haskell.packages.ghc802.override {
+    overrides = self: super: {
+      # To make certain IFD deps survive GC.
+      haskell-prevent-ifd-gc = [] ++
+        buildPackages.haskellPackages.cabal2nix.all;
+    };
+  };
+
+  build = let
+    src = sourceByNegativeRegex (gitignoreToRegexes ./.gitignore ++ ["/config" "/default.nix"]) ./.;
+  in haskell.lib.overrideCabal (haskellPackagesWithOverrides.callCabal2nix "kornel" src {}) (drv: {
+    # For speed:
+    doHaddock = false;
+    # Tests require network.
+    #   • `doCheck = false` would remove test deps from nix-shell → useless,
+    #   • `checkPhase = ""` doesn’t override (see pkgs/development/haskell-modules/generic-builder.nix).
+    checkPhase = ":";
+    libraryHaskellDepends = drv.libraryHaskellDepends ++ [ prevent-ifd-gc ];
   });
+
+  env = lib.overrideDerivation build.env (oldAttrs: {
+    buildInputs = [ git watchexec prevent-ifd-gc ] ++
+      (with haskellPackagesWithOverrides; [ cabal-install hlint hindent stylish-haskell ]);
+
+    # Caution: leave oldAttrs.shellHook in place, or HIE will break (just HIE!).
+    shellHook = oldAttrs.shellHook + ''
+      export NIX_PATH='nixpkgs=${sources.nixpkgs}'
+    '';
+  });
+
+  prevent-ifd-gc = let
+    srcs = lib.attrValues sources ++ haskellPackagesWithOverrides.haskell-prevent-ifd-gc;
+    in writeTextDir "prevent-ifd-gc" (toString srcs + "\n");
 
 in build // { inherit env hie; }
--- hie.sh
+++ hie.sh
@@ -2,9 +2,9 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
-argv=( "$@" )
+argv=( "$@"                     )
 argv=( "${argv[@]/\'/\'\\\'\'}" )
-argv=( "${argv[@]/#/\'}" )
-argv=( "${argv[@]/%/\'}" )
+argv=( "${argv[@]/#/\'}"        )
+argv=( "${argv[@]/%/\'}"        )
 
 exec nix-shell --pure --run "exec $(nix-build -o dist/nix/hie -A hie)/bin/hie ${argv[*]}"
